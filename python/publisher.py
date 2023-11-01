2c91b36591e9b87c3ddebca84a7511a48ce4299a
TheLPCheat <thelpcheat@gmail.com>
2023-11-01 03:25:33 +0100

    bugfix

--- python/publisher.py
+++ python/publisher.py
@@ -32,7 +32,7 @@ class LaserScanPublisher(Node):
         scan = self.lidar.scan()
         if scan:
             # Verify that 'ranges' is a list of floats within the expected range
-            ranges = [float(range_val) for range_val in self.lidar.grid]
+            ranges = [float(range_val) for range_val in self.lidar.range]
         
             laser_scan_msg = LaserScan()
             laser_scan_msg.header.stamp = self.get_clock().now().to_msg()
