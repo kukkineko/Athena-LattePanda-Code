0577d06d3bb07668cdbf5ee2bfdc703bcc1e47b0
TheLPCheat <thelpcheat@gmail.com>
2023-11-21 11:31:40 +0100

    rclnodejs V1 done

--- nodejs/TypecriptServer.ts
+++ nodejs/TypecriptServer.ts
@@ -1,22 +1,92 @@
-// TypeScriptServerClient.ts
+import * as rclnodejs from 'rclnodejs';
 
-import * as net from 'net';
+// Define ROS 2 message types
+const LaserScan = 'sensor_msgs/msg/LaserScan';
+const Imu = 'sensor_msgs/msg/Imu';
 
-export class TypeScriptServerClient {
-    private socket: net.Socket;
+// Create a ROS 2 node
+rclnodejs.init().then(() => {
+    const node = rclnodejs.createNode('typescript_server_node');
 
-    constructor(private host: string = 'localhost', private port: number = 8080) {
-        this.socket = new net.Socket();
-        this.socket.connect(port, host);
+    // Create publishers for lidar and imu topics
+    const lidarPublisher = node.createPublisher(LaserScan, 'lidar_topic');
+    const imuPublisher = node.createPublisher(Imu, 'imu_topic');
+
+    // Function to handle received data and publish to ROS 2
+    function handleData(topic: string, data: any) {
+        switch (topic) {
+            case 'lidar':
+                publishLidarData(data);
+                break;
+            case 'imu':
+                publishImuData(data);
+                break;
+            default:
+                console.error(`Unknown topic: ${topic}`);
+        }
     }
 
-    public send_data(topic: string, data: any): void {
-        const message = { topic, data };
-        const jsonMessage = JSON.stringify(message);
-        this.socket.write(jsonMessage);
+    // Function to publish lidar data
+    function publishLidarData(data: any) {
+        // Assuming data is in the format suitable for LaserScan message
+        const lidarMessage = new rclnodejs.Message(LaserScan, {
+            header: {
+                stamp: { sec: data.header.stamp.sec, nanosec: data.header.stamp.nanosec },
+                frame_id: data.header.frame_id,
+            },
+            angle_min: data.angle_min,
+            angle_max: data.angle_max,
+            angle_increment: data.angle_increment,
+            time_increment: data.time_increment,
+            scan_time: data.scan_time,
+            range_min: data.range_min,
+            range_max: data.range_max,
+            ranges: data.ranges,
+            intensities: data.intensities,
+        });
+
+        lidarPublisher.publish(lidarMessage);
     }
 
-    public close(): void {
-        this.socket.end();
+
+    // Function to publish imu data
+    function publishImuData(data: any) {
+        // Assuming data is in the format suitable for Imu message
+        const imuMessage = new rclnodejs.Message(Imu, {
+            header: { stamp: { sec: data.timestamp, nanosec: 0 }, frame_id: 'imu_frame' },
+            orientation: data.orientation,
+            angular_velocity: data.angular_velocity,
+            linear_acceleration: data.linear_acceleration,
+        });
+
+        imuPublisher.publish(imuMessage);
     }
-}
+
+    // Handle termination signals
+    process.on('SIGINT', handleExit);
+    process.on('SIGTERM', handleExit);
+
+    // Subscribe to data from the Python client
+    const subscription = node.createSubscription('python_data_topic', 'std_msgs/msg/String', (msg) => {
+        try {
+            const data = JSON.parse(msg.data);
+            handleData(data.topic, data.data);
+        } catch (error) {
+            console.error(`Error parsing data: ${error}`);
+        }
+    });
+
+    // Spin the node to handle messages
+    rclnodejs.spin(node);
+
+    // Cleanup on script termination
+    process.on('exit', () => {
+        handleExit();
+    });
+
+    function handleExit() {
+        console.log("Received termination signal. Cleaning up...");
+        // Additional cleanup logic if needed
+        process.exit(0);
+    }
+});
--- python/client.py
+++ python/client.py
@@ -1,26 +1,46 @@
-# client.py
-
 import subprocess
 import json
 import sys
-import rclnodejs
 import atexit
+import time
+import signal
+import logging
+import os
 
 class TypeScriptServerClient:
     def __init__(self):
-        # Start TypeScript server
-        self.ts_server_process = subprocess.Popen(
-            ["node", "../nodejs/typescriptServer.js"],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            text=True,
-        )
+        try:
+            # Start TypeScript server
+            self.ts_server_process = subprocess.Popen(
+                ["node", "../nodejs/typescriptServer.js"],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                preexec_fn=os.setsid  # Create a new process group
+            )
+        except Exception as e:
+            logging.error(f"Error starting TypeScript server: {e}")
+            sys.exit(1)
 
-        # Wait for the server to start (you might want to add better handling)
-        while True:
+        # Wait for the server to start with a timeout
+        max_retries = 10
+        retry_interval = 1  # in seconds
+
+        for _ in range(max_retries):
             line = self.ts_server_process.stdout.readline().strip()
             if line == 'TypeScript server is listening on port 3000':
                 break
+            time.sleep(retry_interval)
+        else:
+            logging.error("Failed to start TypeScript server within the given timeout.")
+            sys.exit(1)
+
+        # Register cleanup function
+        atexit.register(self.close)
+
+        # Register signal handler for termination signals
+        signal.signal(signal.SIGINT, self.handle_exit)
+        signal.signal(signal.SIGTERM, self.handle_exit)
 
     def send_data(self, topic, data):
         # Send data to TypeScript server
@@ -31,22 +51,27 @@ class TypeScriptServerClient:
 
     def close(self):
         # Close TypeScript server
-        self.ts_server_process.terminate()
-        self.ts_server_process.wait()
+        if hasattr(self, 'ts_server_process') and self.ts_server_process.poll() is None:
+            os.killpg(os.getpgid(self.ts_server_process.pid), signal.SIGTERM)
+            self.ts_server_process.wait()
+            logging.info("TypeScript server terminated.")
 
-# Ensure the TypeScript server is closed when the Python script exits
-def cleanup():
-    ts_client.close()
+    def handle_exit(self, signum, frame):
+        # Handle termination signals
+        logging.info("Received termination signal. Cleaning up...")
+        self.close()
+        sys.exit(0)
 
-atexit.register(cleanup)
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == '__main__':
-    ts_client = TypeScriptServerClient()
-
-    # Your existing code or any additional logic can go here
-
-    # Example: sending data
-    ts_client.send_data('imu', {'some': 'data'})
-
-    # Close TypeScript server when the script exits
-    ts_client.close()
+    with TypeScriptServerClient() as ts_client:
+        # Your existing code or any additional logic can go here
+        ts_client.send_data('imu', {'some': 'data'})
