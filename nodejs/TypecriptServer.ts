b80c40b027f3585a05ddfdae41d26ae9d597b7f7
TheLPCheat <thelpcheat@gmail.com>
2023-11-21 12:07:07 +0100

    tsserver korrektur

--- nodejs/TypecriptServer.ts
+++ nodejs/TypecriptServer.ts
@@ -1,8 +1,6 @@
 import * as rclnodejs from 'rclnodejs';
-
-// Define ROS 2 message types
-const LaserScan = 'sensor_msgs/msg/LaserScan';
-const Imu = 'sensor_msgs/msg/Imu';
+import { LaserScan } from 'sensor_msgs/msg/LaserScan';
+import { Imu } from 'sensor_msgs/msg/Imu';
 
 // Create a ROS 2 node
 rclnodejs.init().then(() => {
@@ -29,35 +27,33 @@ rclnodejs.init().then(() => {
     // Function to publish lidar data
     function publishLidarData(data: any) {
         // Assuming data is in the format suitable for LaserScan message
-        const lidarMessage = new rclnodejs.Message(LaserScan, {
-            header: {
-                stamp: { sec: data.header.stamp.sec, nanosec: data.header.stamp.nanosec },
-                frame_id: data.header.frame_id,
-            },
-            angle_min: data.angle_min,
-            angle_max: data.angle_max,
-            angle_increment: data.angle_increment,
-            time_increment: data.time_increment,
-            scan_time: data.scan_time,
-            range_min: data.range_min,
-            range_max: data.range_max,
-            ranges: data.ranges,
-            intensities: data.intensities,
-        });
+        const lidarMessage = new LaserScan();
+        lidarMessage.header.stamp.sec = data.header.stamp.sec;
+        lidarMessage.header.stamp.nanosec = data.header.stamp.nanosec;
+        lidarMessage.header.frame_id = data.header.frame_id;
+        lidarMessage.angle_min = data.angle_min;
+        lidarMessage.angle_max = data.angle_max;
+        lidarMessage.angle_increment = data.angle_increment;
+        lidarMessage.time_increment = data.time_increment;
+        lidarMessage.scan_time = data.scan_time;
+        lidarMessage.range_min = data.range_min;
+        lidarMessage.range_max = data.range_max;
+        lidarMessage.ranges = data.ranges;
+        lidarMessage.intensities = data.intensities;
 
         lidarPublisher.publish(lidarMessage);
     }
 
-
     // Function to publish imu data
     function publishImuData(data: any) {
         // Assuming data is in the format suitable for Imu message
-        const imuMessage = new rclnodejs.Message(Imu, {
-            header: { stamp: { sec: data.timestamp, nanosec: 0 }, frame_id: 'imu_frame' },
-            orientation: data.orientation,
-            angular_velocity: data.angular_velocity,
-            linear_acceleration: data.linear_acceleration,
-        });
+        const imuMessage = new Imu();
+        imuMessage.header.stamp.sec = data.header.stamp.sec;
+        imuMessage.header.stamp.nanosec = data.header.stamp.nanosec;
+        imuMessage.header.frame_id = data.header.frame_id;
+        imuMessage.orientation = data.orientation;
+        imuMessage.angular_velocity = data.angular_velocity;
+        imuMessage.linear_acceleration = data.linear_acceleration;
 
         imuPublisher.publish(imuMessage);
     }
@@ -67,14 +63,23 @@ rclnodejs.init().then(() => {
     process.on('SIGTERM', handleExit);
 
     // Subscribe to data from the Python client
-    const subscription = node.createSubscription('python_data_topic', 'std_msgs/msg/String', (msg) => {
-        try {
-            const data = JSON.parse(msg.data);
-            handleData(data.topic, data.data);
-        } catch (error) {
-            console.error(`Error parsing data: ${error}`);
+    const subscription = node.createSubscription(
+        'python_data_topic',
+        'std_msgs/msg/String',
+        (msg: rclnodejs.Message) => {
+            if (msg instanceof rclnodejs.Message) {
+                // Handle specific message types (e.g., LaserScan, Imu)
+                const data = JSON.parse((msg as any).data);
+                handleData(data.topic, data.data);
+            } else if (typeof msg === 'string') {
+                // Handle string messages
+                console.log('Received string message:', msg);
+            } else {
+                // Handle other types
+                console.error('Received unsupported message type:', msg);
+            }
         }
-    });
+    );
 
     // Spin the node to handle messages
     rclnodejs.spin(node);
