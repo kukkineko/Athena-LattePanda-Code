37e2a9656fe7148bda06b10bb8cbe87299fcf988
Mert Gökay Gürel <thelpcheat@gmail.com>
2023-12-03 17:31:11 +0100

    nonsense

--- nodejs/typecriptServer.ts
+++ nodejs/typecriptServer.ts
@@ -1,89 +1,84 @@
-'strict mode';
-
 import * as rclnodejs from 'rclnodejs';
 
 /**
- * ROS2 publisher for simulated sensor data.
+ * ROS2 publisher of simulated LaserScan messages.
+ * A timer is used to create and publish a laserScan message once per second (1 Hz).
+ * Use start() and stop() to initiate and terminate messgage publication.  
  */
+export class LaserScanPublisher {
 
-export class SensorPublisher {
+  private isRunning = false;
+  private publisher: rclnodejs.Publisher;
+  private publisherTimer: rclnodejs.Timer;
 
-    private isRunning = false;
-    private lidarPublisher: rclnodejs.Publisher;
-    private imuPublisher: rclnodejs.Publisher;
-    private publisherTimer: rclnodejs.Timer;
+  /**
+   * Create an instance
+   * 
+   * @param node - The node that to which this publisher belongs.
+   * @param topic - The topic name to which laserScan msgs will be published.
+   */
+  constructor(public readonly node: rclnodejs.Node, public readonly topic = 'laser_frame') {
+    this.publisher = node.createPublisher('sensor_msgs/msg/LaserScan', topic);
+  }
 
-    /**
-     * Create an instance
-     * 
-     * @param node - The node that to which this publisher belongs.
-     */
-    constructor(public readonly node: rclnodejs.Node) {
-        this.lidarPublisher = node.createPublisher('sensor_msgs/msg/LaserScan', 'laser_frame');
-        this.imuPublisher = node.createPublisher('sensor_msgs/msg/Imu', 'imu_frame');
-    }
+  /**
+   * Start the laserScan message generation and publishing process.
+   * 
+   * @param interval - The unit of time (milliseconds) to wait before running the next .
+   */
+  start(interval = 1000): void {
+    if (this.isRunning) return;
 
-    /**
-     * Start the sensor data generation and publishing process.
-     * 
-     * @param interval - The unit of time (milliseconds) to wait before running the next .
-     */
-    start(interval = 1000): void {
-        if (this.isRunning) return;
+    this.isRunning = true;
 
-        this.isRunning = true;
-
-        this.publisherTimer = this.node.createTimer(interval, () => {
-            const laserScanMsg = this.genLaserScanMsg();
-            const imuMsg = this.genImuMsg();
-
-            console.log('Publishing LaserScan and IMU data');
-            this.lidarPublisher.publish(laserScanMsg);
-            this.imuPublisher.publish(imuMsg);
-        });
-    }
-
-    /**
-     * Stop creating and publishing sensor data.
-     */
-    stop() {
-        if (!this.isRunning) return;
-
-        this.publisherTimer.cancel();
-        this.publisherTimer = null;
-        this.isRunning = false;
-    }
-
-    /**
-     * Creates a simulated forward facing LaserScan message.
-     * @returns A new LaserScan message
-     */
-    protected genLaserScanMsg(): rclnodejs.sensor_msgs.msg.LaserScan {
-        // Similar implementation to your genLaserScanMsg
-    }
-
-    /**
-     * Creates a simulated IMU message.
-     * @returns A new IMU message
-     */
-    protected genImuMsg(): rclnodejs.sensor_msgs.msg.Imu {
-        // Implement your logic to generate IMU data
-    }
-}
-
-async function main() {
-    rclnodejs.init().then(() => {
-        const node = rclnodejs.createNode('sensor_publisher_node');
-        const sensorPublisher = new SensorPublisher(node);
-
-        sensorPublisher.start(1000); // Start publishing sensor data every 1000 milliseconds
-
-        // Handle termination
-        process.on('SIGINT', () => {
-            sensorPublisher.stop();
-            rclnodejs.shutdown();
-        });
+    this.publisherTimer = this.node.createTimer(interval, () => {
+      let msg = this.genLaserScanMsg();
+      console.log('publish msg at ', msg.header.stamp.sec);
+      this.publisher.publish(msg);
     });
-}
+  }
 
-main();
+  /**
+   * Stop creating and publishing laserScan messages.
+   */
+  stop() {
+    this.publisherTimer.cancel();
+    this.publisherTimer = null;
+    this.isRunning = false;
+  }
+
+  /**
+   * Creates a simulated forward facing LaserScan message.
+   * Scan data consists of 180 measurements on a 180 degree arc centered on the
+   * x-axis extending directly forward of the virutal lidar device.
+   * 
+   * @param range  - The distance of simulated lidar range readings
+   * @returns A new LaserScan message
+   */
+  protected genLaserScanMsg(range = 10): rclnodejs.sensor_msgs.msg.LaserScan {
+
+    // create empty laserScan msg
+    let laserScanMsg  = rclnodejs.createMessageObject('sensor_msgs/msg/LaserScan') as 
+      rclnodejs.sensor_msgs.msg.LaserScan;
+
+    // configure msg header 
+    laserScanMsg.header.frame_id = 'laser_frame';
+    laserScanMsg.header.stamp = this.node.now().toMsg();
+
+    // generate range and intensity data
+    let sample_cnt = 180;
+    let ranges = new Array(sample_cnt).fill(range);
+
+    // configure msg data
+    laserScanMsg.angle_min = 0;
+    laserScanMsg.angle_max = Math.PI / 2.0;
+    laserScanMsg.angle_increment = Math.PI / 180.0;
+    laserScanMsg.time_increment = 1.0 / sample_cnt;
+    laserScanMsg.scan_time = 1.0;
+    laserScanMsg.range_min = range - 1;
+    laserScanMsg.range_max = range + 1;
+    laserScanMsg.ranges = ranges;
+
+    return laserScanMsg;
+  }
+}
