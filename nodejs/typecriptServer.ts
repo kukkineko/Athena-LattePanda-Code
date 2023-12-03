8a056b27cfccc5d157300c584e4bc31668f7bc68
TheLPCheat <thelpcheat@gmail.com>
2023-12-03 14:50:39 +0100

    ts

--- nodejs/typecriptServer.ts
+++ nodejs/typecriptServer.ts
@@ -1,100 +1,89 @@
+'strict mode';
+
 import * as rclnodejs from 'rclnodejs';
 
+/**
+ * ROS2 publisher for simulated sensor data.
+ */
 
-// Create a ROS 2 node
-rclnodejs.init().then(() => {
-    const node = rclnodejs.createNode('typescript_server_node');
+export class SensorPublisher {
 
-    // Create publishers for lidar and imu topics
-    const lidarPublisher = node.createPublisher('sensor_msgs/msg/LaserScan', 'lidar_topic');
-    const imuPublisher = node.createPublisher('sensor_msgs/msg/Imu', 'imu_topic');
+    private isRunning = false;
+    private lidarPublisher: rclnodejs.Publisher;
+    private imuPublisher: rclnodejs.Publisher;
+    private publisherTimer: rclnodejs.Timer;
 
-    // Function to handle received data and publish to ROS 2
-    function handleData(topic: string, data: any) {
-        switch (topic) {
-            case 'lidar':
-                publishLidarData(data);
-                break;
-            case 'imu':
-                publishImuData(data);
-                break;
-            default:
-                console.error(`Unknown topic: ${topic}`);
-        }
+    /**
+     * Create an instance
+     * 
+     * @param node - The node that to which this publisher belongs.
+     */
+    constructor(public readonly node: rclnodejs.Node) {
+        this.lidarPublisher = node.createPublisher('sensor_msgs/msg/LaserScan', 'laser_frame');
+        this.imuPublisher = node.createPublisher('sensor_msgs/msg/Imu', 'imu_frame');
     }
 
-    // Function to publish lidar data
-    function publishLidarData(data: any) {
-        // Assuming data is in the format suitable for LaserScan message
-        const lidarMessage = new LaserScan();
-        lidarMessage.header.stamp.sec = data.header.stamp.sec;
-        lidarMessage.header.stamp.nanosec = data.header.stamp.nanosec;
-        lidarMessage.header.frame_id = data.header.frame_id;
-        lidarMessage.angle_min = data.angle_min;
-        lidarMessage.angle_max = data.angle_max;
-        lidarMessage.angle_increment = data.angle_increment;
-        lidarMessage.time_increment = data.time_increment;
-        lidarMessage.scan_time = data.scan_time;
-        lidarMessage.range_min = data.range_min;
-        lidarMessage.range_max = data.range_max;
-        lidarMessage.ranges = data.ranges;
-        lidarMessage.intensities = data.intensities;
+    /**
+     * Start the sensor data generation and publishing process.
+     * 
+     * @param interval - The unit of time (milliseconds) to wait before running the next .
+     */
+    start(interval = 1000): void {
+        if (this.isRunning) return;
 
-        lidarPublisher.publish(lidarMessage);
+        this.isRunning = true;
+
+        this.publisherTimer = this.node.createTimer(interval, () => {
+            const laserScanMsg = this.genLaserScanMsg();
+            const imuMsg = this.genImuMsg();
+
+            console.log('Publishing LaserScan and IMU data');
+            this.lidarPublisher.publish(laserScanMsg);
+            this.imuPublisher.publish(imuMsg);
+        });
     }
 
-    // Function to publish imu data
-    function publishImuData(data: any) {
-        // Assuming data is in the format suitable for Imu message
-        const imuMessage = new Imu();
-        imuMessage.header.stamp.sec = data.header.stamp.sec;
-        imuMessage.header.stamp.nanosec = data.header.stamp.nanosec;
-        imuMessage.header.frame_id = data.header.frame_id;
-        imuMessage.orientation = data.orientation;
-        imuMessage.angular_velocity = data.angular_velocity;
-        imuMessage.linear_acceleration = data.linear_acceleration;
+    /**
+     * Stop creating and publishing sensor data.
+     */
+    stop() {
+        if (!this.isRunning) return;
 
-        imuPublisher.publish(imuMessage);
+        this.publisherTimer.cancel();
+        this.publisherTimer = null;
+        this.isRunning = false;
     }
 
-    // Handle termination signals
-    process.on('SIGINT', handleExit);
-    process.on('SIGTERM', handleExit);
+    /**
+     * Creates a simulated forward facing LaserScan message.
+     * @returns A new LaserScan message
+     */
+    protected genLaserScanMsg(): rclnodejs.sensor_msgs.msg.LaserScan {
+        // Similar implementation to your genLaserScanMsg
+    }
 
-    // Subscribe to data from the Python client
-    const subscription = node.createSubscription(
-        'python_data_topic',
-        'std_msgs/msg/String',
-        (msg: rclnodejs.Message | string) => {
-            if (msg instanceof LaserScan) {
-                // Handle LaserScan message
-                const data = JSON.parse((msg as any).data);
-                handleData(data.topic, data.data);
-            } else if (msg instanceof Imu) {
-                // Handle Imu message
-                const data = JSON.parse((msg as any).data);
-                handleData(data.topic, data.data);
-            } else if (typeof msg === 'string') {
-                // Handle string messages
-                console.log('Received string message:', msg);
-            } else {
-                // Handle other types
-                console.error('Received unsupported message type:', msg);
-            }
-        }
-    );
+    /**
+     * Creates a simulated IMU message.
+     * @returns A new IMU message
+     */
+    protected genImuMsg(): rclnodejs.sensor_msgs.msg.Imu {
+        // Implement your logic to generate IMU data
+    }
+}
 
-    // Spin the node to handle messages
-    rclnodejs.spin(node);
+async function main() {
+    rclnodejs.init().then(() => {
+        const node = rclnodejs.createNode('sensor_publisher_node');
+        const sensorPublisher = new SensorPublisher(node);
 
-    // Cleanup on script termination
-    process.on('exit', () => {
-        handleExit();
+        sensorPublisher.start(1000); // Start publishing sensor data every 1000 milliseconds
+
+        // Handle termination
+        process.on('SIGINT', () => {
+            sensorPublisher.stop();
+            rclnodejs.shutdown();
+        });
     });
+}
 
-    function handleExit() {
-        console.log("Received termination signal. Cleaning up...");
-        // Additional cleanup logic if needed
-        process.exit(0);
-    }
-});
+main();
